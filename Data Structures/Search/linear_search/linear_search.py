cfe7c6ac67dd478ec70d79eb722cc37a8216a68b
shantanuclass <anantwarsr@rknec.edu>
2024-08-06 07:15:24 +0000

    .

--- Data Structures/Search/linear_search/linear_search.py
+++ Data Structures/Search/linear_search/linear_search.py
@@ -2,6 +2,6 @@ array = [25 , 50 , 75 , 100]
 
 target = int(input("Enter the Target : "))
 
-for ele in range(0,array.length()):
-    if ele == target:
+for ele in range(0,len(array)):
+    if array[ele] == target:
         print(f"target found at index {ele}")
